431358bd02b00e2df0e2a9b090f577d8b4042c15
tangqian <275855127@qq.com>
2015-05-29 17:59:05 +0800

    删除多余文件

---
+++ src/main/java/com/tq/management/TestController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright(c) 2015 gvtv.com.cn All rights reserved.
+ * distributed with this file and available online at
+ * http://www.gvtv.com.cn/
+ */
+package com.tq.management;
+
+/**
+ * @version 1.0
+ * @author tangqian
+ */
+public class TestController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/tq/management/Test.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright(c) 2015 gvtv.com.cn All rights reserved.
+ * distributed with this file and available online at
+ * http://www.gvtv.com.cn/
+ */
+package com.tq.management;
+
+/**
+ * @version 1.0
+ * @author tangqian
+ */
+public class Test {
+
+}
