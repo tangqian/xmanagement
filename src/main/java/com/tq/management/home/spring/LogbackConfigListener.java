ca07657d3225d29705bf8d5e9dbc5a2fff794aec
tangqian <275855127@qq.com>
2015-06-03 17:53:35 +0800

    修改数据源为Druid及修改logback配置

---
+++ src/main/java/com/tq/management/home/spring/LogbackConfigListener.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright(c) 2015 gvtv.com.cn All rights reserved.
+ * distributed with this file and available online at
+ * http://www.gvtv.com.cn/
+ */
+package com.tq.management.home.spring;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+/**
+ * @version 1.0
+ * @author tangqian
+ */
+public class LogbackConfigListener implements ServletContextListener {
+	private static final String CONFIG_LOCATION = "logbackConfigLocation";
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		// 从web.xml中加载指定文件名的日志配置文件
+		String logbackConfigLocation = event.getServletContext().getInitParameter(CONFIG_LOCATION);
+		String fn = LogbackConfigListener.class.getResource(logbackConfigLocation).getPath();
+		try {
+			LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+			loggerContext.reset();
+			JoranConfigurator joranConfigurator = new JoranConfigurator();
+			joranConfigurator.setContext(loggerContext);
+			joranConfigurator.doConfigure(fn);
+		} catch (JoranException e) {
+			System.err.println("can loading logback configure file from " + fn);
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+}
